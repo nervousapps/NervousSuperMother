c3f077c1f3f4076495825afd19855ea0d2b077b5
nerousapps <nervousrvoltage@gmail.com>
2021-03-11 23:42:24 +0100

    Fisrt commit, to test !

--- lib/NervousSuperMother/hardware/pins.h
+++ lib/NervousSuperMother/hardware/pins.h
@@ -5,60 +5,65 @@
 #define MIDI_IN	    0
 
 // PT8211 DAC
-#define BCK_DAC	    9
-#define U7_DIN	    22
-#define U7_WS	      23
+#define BCK_DAC	    4
+#define U7_DIN	    2
+#define U7_WS	      3
 
 // Encoders
-#define ENC1_1	      2
-#define ENC1_2	      3
-#define ENC1_SW	      4
-#define ENC2_1	      6
-#define ENC2_2	      5
-#define ENC2_SW	      7
+#define ENC1_1	      1
+#define ENC1_2	      29
+#define ENC1_SW	      30
+#define ENC2_1	      5
+#define ENC2_2	      28
+#define ENC2_SW	      9
 
 // Triggers
-#define TRIG1	        8
-#define TRIG2	        10
-#define TRIG3	        11
-#define TRIG4	        12
-#define TRIG5	        38
-#define TRIG6	        39
+#define TRIG1	        10
+#define TRIG2	        11
+#define CV1	          A0
+#define CV2	          A2
+#define CV3	          A3
+#define CV4	          A8
 
 // Display
-#define DISPLAY_RS	  24
-#define DISPLAY_RW	  25
-#define DISPLAY_E	    26
-#define DISPLAY_DB4	  27
-#define DISPLAY_DB5	  28
-#define DISPLAY_DB6	  29
-#define DISPLAY_DB7	  30
+#define DISPLAY_RS	  31
+#define DISPLAY_RW	  32
+#define DISPLAY_E	    33
+#define DISPLAY_DB4	  34
+#define DISPLAY_DB5	  35
+#define DISPLAY_DB6	  36
+#define DISPLAY_DB7	  37
 
-// Potentiometers
-#define POT1	        A12
-#define POT2	        A13
-#define POT3	        A14
-#define POT4	        A15
-#define POT5	        A16
+//Mux Pins
+#define MS0 A11
+#define MS1 A12
+#define MS2 A13
+#define MS3 A14
+#define MZ A10
+#define MnumControls 15
 
 // Switches
-#define SW1	          A17
-#define SW2	          A18
-// Teensy 3.5
-// #define SW3	          A25
-// Teensy 3.6
-#define SW3	          A23
+#define SW1	          A15
+#define SW2	          A16
+#define SW3	          A17
 
 // Sliders
-#define SLIDE1	      A0
-#define SLIDE2	      A1
-#define SLIDE3	      A2
-#define SLIDE4	      A3
-#define SLIDE5	      A4
-#define SLIDE6	      A5
-#define SLIDE7	      A6
-#define SLIDE8	      A7
-#define SLIDE9	      A10
-#define SLIDE10	      A11
+#define SLIDE1	      0
+#define SLIDE2	      1
+#define SLIDE3	      2
+#define SLIDE4	      3
+#define SLIDE5	      4
+#define SLIDE6	      5
+#define SLIDE7	      6
+#define SLIDE8	      7
+#define SLIDE9	      8
+#define SLIDE10	      9
+
+// Potentiometers
+#define POT1	        10
+#define POT2	        11
+#define POT3	        12
+#define POT4	        13
+#define POT5	        14
 
 #endif

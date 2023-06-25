5408a5690419aac36b72bd3ac9a1ffa27669cf8e
nervousapps <achille.penet@icloud.com>
2023-06-26 00:54:24 +0200

    First commit

---
+++ src/custom-objects/AudioUtility.h
@@ -0,0 +1,22 @@
+#ifndef AUDIO_UTILITY_H
+#define AUDIO_UTILITY_H
+
+#include "Arduino.h"
+
+// * Data types
+
+// Represents a single audio block of int16_t samples
+struct audio_block_data_t
+{
+    int16_t data[AUDIO_BLOCK_SAMPLES];
+};
+
+// * Utility functions
+
+inline float dBtoGain(float dB) { return pow10f(dB * 0.05); }
+
+inline float gaintodB(float gain) { return 20 * log10f(gain); }
+
+inline float noteToFreq(uint8_t note) { return powf(2, (float)(note - 69) / 12.0f) * 440.0f; }
+
+#endif
---
+++ src/custom-objects/CustomAudioObjects.h
@@ -0,0 +1,9 @@
+#ifndef CUSTOMAUDIOOBJECTS_H
+#define CUSTOMAUDIOOBJECTS_H
+
+// list all custom audio objects here
+#include "record_sample.h"
+#include "record_clip.h"
+#include "play_clip.h"
+
+#endif
